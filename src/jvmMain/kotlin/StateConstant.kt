9a40bdbb25a85b978e05b88c80de96907ca60698
opoojkk <33021407+opoojkk@users.noreply.github.com>
2023-06-17 16:01:11 +0800

    feature:change hint text content

--- src/jvmMain/kotlin/StateConstant.kt
+++ src/jvmMain/kotlin/StateConstant.kt
@@ -2,9 +2,9 @@ enum class StateConstant {
     Normal,
     NotEnoughOptions("选项不够还不让重复，\n怎么生成呢?"),
     FilePathError("文件路径有问题还是没有选文件"),
-    CantFindFile("小笨蛋，刚选的刚选的可不是一个文件"),
+    CantFindFile("刚选的刚选的可不是一个文件"),
     DeselectFile("没有选择文件呀"),
-    WrongFileFormat("小笨蛋，Excel格式的文件");
+    WrongFileFormat("得是xlsx格式的文件");
 
 
     val content: String
